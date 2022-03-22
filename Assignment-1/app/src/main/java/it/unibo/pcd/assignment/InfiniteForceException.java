5c37cce09358e71697cf6a7a4d0520e6a67b0187
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-03-22 17:29:18 +0100

    Import init files and fix its import related problems

---
+++ Assignment-1/app/src/main/java/it/unibo/pcd/assignment/InfiniteForceException.java
@@ -0,0 +1,5 @@
+package it.unibo.pcd.assignment;
+
+public class InfiniteForceException extends Exception {
+
+}
---
+++ Assignment-1/app/src/main/java/it/unibo/pcd/assignment/NullVectorException.java
@@ -0,0 +1,5 @@
+package it.unibo.pcd.assignment;
+
+public class NullVectorException extends Exception {
+
+}
