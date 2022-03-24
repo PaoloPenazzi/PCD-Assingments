d18d45f2706881816a0892d8dba673a3c325e0f8
paolo <paolo.penazzi@studio.unibo.it>
2022-03-24 09:33:40 +0100

    Refactor Velocity2d class

--- Assignment-1/src/main/java/it/unibo/pcd/assignment/model/Velocity2d.java
+++ Assignment-1/src/main/java/it/unibo/pcd/assignment/model/Velocity2d.java
@@ -1,77 +1,70 @@
 /*
- *   V2d.java
+ * V2d.java
  *
  * Copyright 2000-2001-2002  aliCE team at deis.unibo.it
  *
  * This software is the proprietary information of deis.unibo.it
  * Use is subject to license terms.
- *
  */
 package it.unibo.pcd.assignment.model;
 
 import it.unibo.pcd.assignment.NullVectorException;
 
 /**
- *
  * 2-dimensional vector
  * objects are completely state-less
- *
  */
 public class Velocity2d {
+    public double x;
+    public double y;
 
-    public double x,y;
-
-    public Velocity2d(double x, double y){
+    public Velocity2d(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public Velocity2d(Velocity2d v){
+    public Velocity2d(Velocity2d v) {
         this.x = v.x;
         this.y = v.y;
     }
 
-    public Velocity2d(Position2d from, Position2d to){
+    public Velocity2d(Position2d from, Position2d to) {
         this.x = to.getX() - from.getX();
         this.y = to.getY() - from.getY();
     }
 
     public Velocity2d scalarMul(double k) {
-    	x *= k;
-    	y *= k;
-    	return this;
+        x *= k;
+        y *= k;
+        return this;
     }
-    
-    public Velocity2d sum(Velocity2d v) {
-    	x += v.x;
-    	y += v.y;
-    	return this;
-    }
-    
-    public Velocity2d normalize() throws NullVectorException {
-    	double mod =  Math.sqrt(x*x + y*y);
-    	if (mod > 0) {
-    		x /= mod;
-    		y /= mod;
-    		return this;
-    	} else {
-    		throw new NullVectorException();
-    	}
 
+    public void sum(Velocity2d v) {
+        x += v.x;
+        y += v.y;
     }
-    public Velocity2d change(double x, double y) {
-    	this.x = x;
-    	this.y = y;
-    	return this;
+
+    public Velocity2d normalize() throws NullVectorException {
+        double mod = Math.sqrt(x * x + y * y);
+        if (mod > 0) {
+            x /= mod;
+            y /= mod;
+            return this;
+        } else {
+            throw new NullVectorException();
+        }
     }
-    
+
+    public void change(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
     public double getX() {
-    	return x;
+        return x;
     }
 
     public double getY() {
-    	return y;
+        return y;
     }
-    
-    
 }
