06293fb81641614f11ceeef5d0d933cca6d69e1e
paolo <paolo.penazzi@studio.unibo.it>
2022-03-24 09:30:50 +0100

    Refactor Position2d class

--- Assignment-1/src/main/java/it/unibo/pcd/assignment/model/Position2d.java
+++ Assignment-1/src/main/java/it/unibo/pcd/assignment/model/Position2d.java
@@ -1,18 +1,17 @@
 package it.unibo.pcd.assignment.model;
 
 public class Position2d {
-
-    private double x, y;
+    private double x;
+    private double y;
 
     public Position2d(double x, double y){
         this.x = x;
         this.y = y;
     }
 
-    public Position2d sum(Velocity2d v) {
+    public void sum(Velocity2d v) {
     	x += v.x;
     	y += v.y;
-    	return this;
     }
      
     public void change(double x, double y){
