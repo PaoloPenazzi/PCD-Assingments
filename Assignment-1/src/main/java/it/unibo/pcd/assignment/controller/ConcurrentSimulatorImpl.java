405628738f7898b17b62b4621190ec3f7670f8af
paolo <paolo.penazzi@studio.unibo.it>
2022-04-01 13:02:05 +0200

    remove commented code to calculate task execution times. not nessary anymore

--- Assignment-1/src/main/java/it/unibo/pcd/assignment/controller/ConcurrentSimulatorImpl.java
+++ Assignment-1/src/main/java/it/unibo/pcd/assignment/controller/ConcurrentSimulatorImpl.java
@@ -12,31 +12,18 @@ public class ConcurrentSimulatorImpl extends AbstractConcurrentSimulator {
     @Override
     public void run() {
         long iteration = 0;
-        //long createTime = 0;
-        //long startTime = 0;
-        //long barrierTime = 0;
         while (iteration < super.getNumSteps()) {
             super.createLatch();
-            //long start = System.currentTimeMillis();
             this.createWorkers(super.getWorkers().length);
-            //long create = System.currentTimeMillis();
             for (Worker worker : super.getWorkers()) {
                 worker.start();
             }
-            //long compute = System.currentTimeMillis();
             try {
                 super.getLatch().await();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            //long finale = System.currentTimeMillis();
-            /*createTime += create - start;
-            startTime += compute - create;
-            barrierTime += finale - compute;*/
             iteration++;
         }
-        /*System.out.println("Time for creation: " + createTime + "ms");
-        System.out.println("Time for starting: " + startTime + "ms");
-        System.out.println("Time for barrier: " + barrierTime + "ms");*/
     }
 }
