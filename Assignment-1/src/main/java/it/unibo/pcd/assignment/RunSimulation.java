a3f41776317b37bad9513e1a8dbabf24e0d4d058
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-04-03 15:24:31 +0200

    Delete baseView useless class

--- Assignment-1/src/main/java/it/unibo/pcd/assignment/RunSimulation.java
+++ Assignment-1/src/main/java/it/unibo/pcd/assignment/RunSimulation.java
@@ -7,7 +7,7 @@ public class RunSimulation {
 
     public static void main(String[] args) {
         int nWorkers = Runtime.getRuntime().availableProcessors() + 1;
-        Thread simulator = new ConcurrentSimulatorImplWithGUI(1000, 10000, 8, nWorkers);
+        Thread simulator = new SequentialSimulatorImplWithGUI(1000, 10000, 8);
         //Thread simulator = new SequentialSimulatorImpl(1000, 10000, 8);
         long startTime = System.currentTimeMillis();
         simulator.start();
