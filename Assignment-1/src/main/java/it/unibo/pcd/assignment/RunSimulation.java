b9d2f7a51c9293ed2a31d76e4909fda30555ca07
paolo <paolo.penazzi@studio.unibo.it>
2022-04-06 17:38:42 +0200

    Fix run class.

--- Assignment-1/src/main/java/it/unibo/pcd/assignment/RunSimulation.java
+++ Assignment-1/src/main/java/it/unibo/pcd/assignment/RunSimulation.java
@@ -6,16 +6,7 @@ public class RunSimulation {
 
     public static void main(String[] args) {
         int nWorkers = Runtime.getRuntime().availableProcessors() + 1;
-<<<<<<< HEAD
         Thread simulator = new ConcurrentSimulatorImplWithGUI(1000, 10000, 8, 4);
-=======
-<<<<<<< HEAD
-        Thread simulator = new ConcurrentSimulatorImpl(100, 200000, 16, 9);
-        //Thread simulator = new SequentialSimulatorImpl(1000, 10000, 8);
-=======
-        Thread simulator = new ConcurrentSimulatorImpl(2, 2, 8, 2);
->>>>>>> d7a016780dff06d4ffd859c349f13dcef2f41630
->>>>>>> 681512abd0adf51088d41f327067fed10b04870b
         long startTime = System.currentTimeMillis();
         simulator.start();
         try {
