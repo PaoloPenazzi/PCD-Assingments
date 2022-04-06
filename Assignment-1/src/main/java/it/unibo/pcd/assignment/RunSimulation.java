862021fc900d1fef7bf5e39850f448847fc3b573
paolo <paolo.penazzi@studio.unibo.it>
2022-04-06 16:54:43 +0200

    Change GUI stamp with new names. vT => virtualTime

--- Assignment-1/src/main/java/it/unibo/pcd/assignment/RunSimulation.java
+++ Assignment-1/src/main/java/it/unibo/pcd/assignment/RunSimulation.java
@@ -6,7 +6,7 @@ public class RunSimulation {
 
     public static void main(String[] args) {
         int nWorkers = Runtime.getRuntime().availableProcessors() + 1;
-        Thread simulator = new ConcurrentSimulatorImplWithGUI(1000, 60000, 8, 4);
+        Thread simulator = new ConcurrentSimulatorImplWithGUI(1000, 10000, 8, 4);
         long startTime = System.currentTimeMillis();
         simulator.start();
         try {
