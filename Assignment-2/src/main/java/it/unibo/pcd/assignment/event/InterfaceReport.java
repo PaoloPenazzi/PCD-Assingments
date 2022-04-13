5c12dd6479838fdbdb3749bbd4f7806ebd309962
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-04-13 17:59:14 +0200

    First test with Java Parser

--- Assignment-2/src/main/java/it/unibo/pcd/assignment/event/InterfaceReport.java
+++ Assignment-2/src/main/java/it/unibo/pcd/assignment/event/InterfaceReport.java
@@ -1,4 +1,11 @@
 package it.unibo.pcd.assignment.event;
 
+import java.util.List;
+
 public interface InterfaceReport {
+    String getFullInterfaceName();
+
+    String getSrcFullFileName();
+
+    List<String> getAllMethodsName();
 }
