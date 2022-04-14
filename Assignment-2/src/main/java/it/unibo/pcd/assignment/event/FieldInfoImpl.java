05d487512727cdacf926dd71eb305a8671dbf326
paolo <paolo.penazzi@studio.unibo.it>
2022-04-14 10:45:45 +0200

    Create FieldInfoImpl class.

---
+++ Assignment-2/src/main/java/it/unibo/pcd/assignment/event/FieldInfoImpl.java
@@ -0,0 +1,34 @@
+package it.unibo.pcd.assignment.event;
+
+public class FieldInfoImpl implements FieldInfo {
+    private String name;
+    private String type;
+    private ClassReport parentClass;
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getFieldTypeFullName() {
+        return this.type;
+    }
+
+    @Override
+    public ClassReport getParent() {
+        return this.parentClass;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setParentClass(ClassReport parentClass) {
+        this.parentClass = parentClass;
+    }
+}
