dec9741d8cbc13ef00bd58e4dccc0eb85bb842b7
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-04-20 11:47:23 +0200

    Simple code refactor

--- Assignment-2/src/main/java/it/unibo/pcd/assignment/event/collector/ClassCollector.java
+++ Assignment-2/src/main/java/it/unibo/pcd/assignment/event/collector/ClassCollector.java
@@ -1,5 +1,6 @@
 package it.unibo.pcd.assignment.event.collector;
 
+import com.github.javaparser.Position;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import it.unibo.pcd.assignment.event.report.ClassReportImpl;
@@ -13,36 +14,41 @@ public class ClassCollector extends VoidVisitorAdapter<ClassReportImpl> {
     @Override
     public void visit(ClassOrInterfaceDeclaration dec, ClassReportImpl collector) {
         super.visit(dec, collector);
-        // name
         collector.setFullClassName(dec.getNameAsString());
-        // src path
-        collector.setSrcFullFileName(dec.getFullyQualifiedName().get());
-        // info on methods
+        collector.setSrcFullFileName(dec.getFullyQualifiedName().orElse("NULL!"));
+
         List<MethodInfoImpl> methodInfoList = new ArrayList<>();
+        this.createMethodInfoList(dec, methodInfoList);
+
+        List<FieldInfoImpl> fieldInfoList = new ArrayList<>();
+        this.createFieldInfoList(dec, fieldInfoList);
+
+        methodInfoList.forEach(m -> m.setParentClass(collector));
+        fieldInfoList.forEach(f -> f.setParentClass(collector));
+        collector.setMethodsInfo(methodInfoList);
+        collector.setFieldsInfo(fieldInfoList);
+    }
+
+    private void createMethodInfoList(ClassOrInterfaceDeclaration dec, List<MethodInfoImpl> methodInfoList){
         dec.getMethods().forEach(m -> {
             MethodInfoImpl methodInfo = new MethodInfoImpl();
             methodInfo.setMain(m.isStatic() && m.getNameAsString().equals("main")
                     && m.getParameterByType(String[].class).isPresent()
                     && m.getParameters().size() == 1);
             methodInfo.setModifiers(m.getModifiers().toString());
-            methodInfo.setBeginLine(m.getBegin().get().line);
-            methodInfo.setEndBeginLine(m.getEnd().get().line);
+            methodInfo.setBeginLine(m.getBegin().orElse(new Position(-1, -1)).line);
+            methodInfo.setEndBeginLine(m.getEnd().orElse(new Position(-1, -1)).line);
             methodInfo.setName(m.getNameAsString());
             methodInfoList.add(methodInfo);
         });
-        // info on fields
-        List<FieldInfoImpl> fieldInfoList = new ArrayList<>();
+    }
+
+    private void createFieldInfoList(ClassOrInterfaceDeclaration dec, List<FieldInfoImpl> fieldInfoList){
         dec.getFields().forEach(f -> {
             FieldInfoImpl fieldInfo = new FieldInfoImpl();
             fieldInfo.setName(f.getVariable(0).getName().asString());
-            // getVariable() returns a list because it's possible to declare more fields in the same line.
             fieldInfo.setType(f.getElementType().asString());
             fieldInfoList.add(fieldInfo);
         });
-        // settato qui perchè si deve richiamare solo quando ha completato il class report
-        methodInfoList.forEach(m -> m.setParentClass(collector));
-        fieldInfoList.forEach(f -> f.setParentClass(collector));
-        collector.setMethodsInfo(methodInfoList);
-        collector.setFieldsInfo(fieldInfoList);
     }
 }
