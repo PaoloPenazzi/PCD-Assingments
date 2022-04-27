d26b4b6cef9ebec02a07e4e77d007722c7254c85
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-04-27 12:27:15 +0200

    Fix test for tree print mode

--- Assignment-2/src/test/java/it/unibo/pcd/assignment/event/TestAsyncPackageReport.java
+++ Assignment-2/src/test/java/it/unibo/pcd/assignment/event/TestAsyncPackageReport.java
@@ -1,5 +1,6 @@
 package it.unibo.pcd.assignment.event;
 
+import hu.webarticum.treeprinter.SimpleTreeNode;
 import io.vertx.core.Future;
 import it.unibo.pcd.assignment.event.report.PackageReport;
 
@@ -7,7 +8,8 @@ public class TestAsyncPackageReport {
 
     public static void main(String[] args) {
         ProjectAnalyzer projectAnalyzer = new ProjectAnalyzerImpl();
-        Future<PackageReport> reportFuture = projectAnalyzer.getPackageReport("it.unibo.pcd.assignment.event.report", System.out::println);
+        Future<PackageReport> reportFuture = projectAnalyzer.getPackageReport("it.unibo.pcd." +
+                "assignment.event.report", System.out::println, new SimpleTreeNode("Test"));
     }
 
 }
