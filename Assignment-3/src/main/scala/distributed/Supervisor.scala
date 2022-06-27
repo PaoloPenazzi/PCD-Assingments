f081b8a454e4facae3f9702860560865431f5226
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-06-27 11:50:51 +0200

    Add demo main and partial implements of Supervisior behavior

--- Assignment-3/src/main/scala/distributed/Supervisor.scala
+++ Assignment-3/src/main/scala/distributed/Supervisor.scala
@@ -9,15 +9,23 @@ import scala.language.postfixOps
 import scala.util.Random
 
 object Supervisor :
-  def apply(): Behavior[Nothing] = Behaviors.setup { ctx =>
+  def apply(id: String,
+            position: (Int, Int)): Behavior[Nothing] = Behaviors.setup { ctx =>
     val cluster = Cluster(ctx.system)
 
     if (cluster.selfMember.hasRole("GUI")) ???
-    else if (cluster.selfMember.hasRole("SENSOR")) ???
-    else ???
+    else if (cluster.selfMember.hasRole("SENSOR")) ctx.spawn(SensorActor((position, id)))
+    else ctx.spawn(FireStationActor((position, id)))
     Behaviors.empty
   }
 
 
 @main def demo: Unit =
-  ???
+  val cityGrid = CityGrid(300, 300)
+  cityGrid.createCityGrid(3, 3)
+  for
+    x <- 8081 to (8081 + cityGrid.zones.size)
+    z <- cityGrid.zones
+  yield
+    startupWithRole("SENSOR", x)(Supervisor("Sensor"+ cityGrid.zones.indexOf(z),
+      (Random.between(z.bounds.x0.toInt, z.bounds.x1.toInt), Random.between(z.bounds.y0.toInt, z.bounds.y1.toInt))))
